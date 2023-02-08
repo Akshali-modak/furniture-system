c10ad5a1193cf802c055365f58339c8d79226760
bootcoding#123 <ModakAkshali211@gmail.com>
2023-02-08 22:29:11 +0530

    spring boot rest api app on customer discount

--- src/main/java/com/bootcoding/furniture/controller/CustomerController.java
+++ src/main/java/com/bootcoding/furniture/controller/CustomerController.java
@@ -2,8 +2,11 @@ package com.bootcoding.furniture.controller;
 
 import com.bootcoding.furniture.modal.Customer;
 import com.bootcoding.furniture.service.CustomerService;
+import com.bootcoding.match.model.Player;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +20,10 @@ public class CustomerController {
     public List<Customer> getAllCustomer(){
 
         return customerService.getAllCustomer();
+    }
+    @PostMapping("/addCustomer")
+    public Customer createCustomer(@RequestBody Customer addCustomer){
+        customerService.getAllCustomer();
+        return addCustomer;
     }
 }
