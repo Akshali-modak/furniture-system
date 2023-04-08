32b7f1d68f9090a9ac5fcb6c45d5aa99258a0872
bootcoding#123 <ModakAkshali211@gmail.com>
2023-04-08 21:32:26 +0530

    spring boot rest api app on customer discount

--- src/main/java/com/bootcoding/match/model/Player.java
+++ src/main/java/com/bootcoding/match/model/Player.java
@@ -9,16 +9,17 @@ public class Player {
    public String getType() {
       return type;
    }
-
    public void setType(String type) {
+
       this.type = type;
    }
 
    public String getName() {
+
       return name;
    }
-
    public void setName(String name) {
+
       this.name = name;
    }
 }
